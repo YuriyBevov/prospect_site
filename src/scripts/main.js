034cf1fb647d2c1620ee8b43c7aab5221fa64bca
Yuriy Bevov <jury19y@mail.ru>
2022-09-17 16:14:44 +0300

    form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
+import "./modules/video";
 import "./modules/accordeon";
 import "./modules/yandexMap";
 import "./modules/select";
+import "./modules/form-mask";
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('LOADED');
---
+++ src/scripts/modules/video.js
@@ -0,0 +1,21 @@
+const videos = document.querySelectorAll('video');
+
+if(videos) {
+  videos.forEach(video => {
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach( entry => {
+        if(entry.isIntersecting) {
+          video.play('muted');
+        } else {
+          if(video.currentTime > 0){
+            video.pause();
+          }
+        }
+      });
+    });
+
+    observer.observe(video);
+  });
+
+}
